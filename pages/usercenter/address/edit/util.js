f64823f4f9bd29d70b5026577047e0addf1a7e66
jiasr <jiasirui3@163.com>
2026-05-31 23:33:33 +0800

    260531

--- pages/usercenter/address/edit/util.js
+++ pages/usercenter/address/edit/util.js
@@ -9,19 +9,46 @@ export const getAddressPromise = () => {
     rejecter = reject;
   });
 
-  addressPromise.push({ resolver, rejecter });
+  addressPromise.push({
+    resolver,
+    rejecter
+  });
 
   return nextPromise;
 };
 
 /** 用户保存了一个地址 */
-export const resolveAddress = (address) => {
+export const resolveAddress = (address, app) => {
   const allAddress = [...addressPromise];
   addressPromise = [];
 
   console.info('用户保存了一个地址', address);
+  wx.request({
+    url: app.globalData.baseUrl + '/v1/address/add',
+    method: 'POST',
+    data: {
+      userid: app.globalData.userid,
+      address: address
+    },
+    timeout: 60000,
+    header: {
+      'content-type': 'application/json'
+    },
+    success: (response) => {
+      console.log('后端请求成功：', response.data.resData.data);
+      // 延迟处理
+      allAddress.forEach(({
+        resolver
+      }) => resolver(address));
 
-  allAddress.forEach(({ resolver }) => resolver(address));
+    },
+    fail(err) {
+      console.log("后端请求失败", err);
+    }
+  });
+
+
+
 };
 
 /** 取消编辑 */
@@ -29,5 +56,7 @@ export const rejectAddress = () => {
   const allAddress = [...addressPromise];
   addressPromise = [];
 
-  allAddress.forEach(({ rejecter }) => rejecter(new Error('cancel')));
+  allAddress.forEach(({
+    rejecter
+  }) => rejecter(new Error('cancel')));
 };
